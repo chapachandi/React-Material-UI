c1975ac46c080eac43248d1055cc8009bd978e2a
chapachandi <chapachandi23@gmail.com>
2021-10-25 01:23:25 +0530

    router created

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Item from './components/Item';
-import Order from './components/Order';
+
 
 export default function App() {
   return (
@@ -16,7 +16,6 @@ export default function App() {
             <Route exact path="/" component={Home} />
             <Route path="/customer" component={Customer}/>
             <Route path="/item" component={Item}/>
-            <Route path="/order" component={Order}/>
             <Route path="/login" component={Login}/>
             <Route path="/signup" component={SignUp}/>
           </Switch>
